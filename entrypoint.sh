a8f8f003c2959c52110023590a44e7b3af12292c
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2019-08-24 17:32:38 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh -c "$JEST_CMD $* --ci --testLocationInResults --json --outputFile=${OUT_PATH}report.json" &> /dev/null
+sh -c "$JEST_CMD $* --ci --testLocationInResults --json --outputFile=report.json" &> /dev/null
 set -e
-sh -c "cat ${OUT_PATH}report.json | /usr/bin/jest-action"
+sh -c "$OUTPUT_CMD cat report.json | /usr/bin/jest-action"
